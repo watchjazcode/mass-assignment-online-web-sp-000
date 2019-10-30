a91c06aba9a37414bfa2fdcb0639bf9326ff729d
Jazmin M <jazminmerckel@gmail.com>
2019-10-30 04:17:28 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,6 +1,8 @@
 class Person
 
-  attr_accessor :name, :age, :location, :user_name
+  attr_accessor :name, :birthday, :hair_color, :eye_color, :height, 
+:weight, :handed, :complexion, :t_shirt_size, 
+:wrist_size, :glove_size, :pant_length, :pant_width
  
   def initialize(user_name:, name:, age:, location:)
     @user_name = user_name
