0e223215e22a9afaf72855f1406ca193a34aa6e3
Jazmin M <jazminmerckel@gmail.com>
2019-10-30 04:17:38 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -5,10 +5,9 @@ class Person
 :wrist_size, :glove_size, :pant_length, :pant_width
  
   def initialize(user_name:, name:, age:, location:)
-    @user_name = user_name
-    @name = name
-    @location = location
-    @age = age
+    :name, :birthday, :hair_color, :eye_color, :height, 
+:weight, :handed, :complexion, :t_shirt_size, 
+:wrist_size, :glove_size, :pant_length, :pant_width
   end
 end
 end
