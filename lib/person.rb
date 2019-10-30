413fa52346e622c2864854c8640e008561a11bef
Jazmin M <jazminmerckel@gmail.com>
2019-10-30 04:25:15 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -2,7 +2,7 @@ class Person
 
   attr_accessor :name, :birthday, :hair_color, :eye_color, :height, :weight, :handed, :complexion, :t_shirt_size,:wrist_size, :glove_size, :pant_length, :pant_width
  
-  def initialize(user_name:, name:, age:, location:)
+  def initialize(name:, birthday:, hair_color:, eye_color:, height:, weight:, handed:, complexion:, t_shirt_size:, wrist_size:, glove_size:, pant_length:, pant_width:)
     @name = name 
     @birthday = birthday
     @hair_color = hair_color
